ba82756b2e0eaf7928d3e377adeb8175d62bcf4a
strayneko <bayurendi10@gmail.com>
2022-09-01 09:38:25 +0700

    :hammer: vercel deploy

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const routes = require('./iamyou/routes')
-const { iamyouRoot } = require('./iamyou/controllers')
+const routes = require('./routes')
+const { iamyouRoot } = require('./controllers')
 
 const app = express()
 
